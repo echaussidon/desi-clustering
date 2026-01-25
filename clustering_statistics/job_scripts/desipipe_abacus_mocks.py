2131b47501d4399ea2598b6aa8b109029067ae70
arnaud de mattia <adematti@dphppcz231.extra.cea.fr>
2026-01-25 19:39:13 +0100

    adding window matrix

--- clustering_statistics/job_scripts/desipipe_abacus_mocks.py
+++ clustering_statistics/job_scripts/desipipe_abacus_mocks.py
@@ -1,42 +1,40 @@
 """
-Script to create and spawn desipipe tasks to compute clustering measurements on HOLI mocks.
+Script to create and spawn desipipe tasks to compute clustering measurements on abacus mocks.
 To create and spawn the tasks on NERSC, use the following commands:
 ```bash
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
-python desipipe_holi_mocks.py  # create the list of tasks
-desipipe tasks -q holi_mocks  # check the list of tasks
-desipipe spawn -q holi_mocks --spawn  # spawn the jobs
-desipipe queues -q holi_mocks  # check the queue
+python desipipe_abacus_mocks.py  # create the list of tasks
+desipipe tasks -q abacus_mocks  # check the list of tasks
+desipipe spawn -q abacus_mocks --spawn  # spawn the jobs
+desipipe queues -q abacus_mocks  # check the queue
 ```
 """
 import os
 from pathlib import Path
 import functools
-import sys
 
 import numpy as np
 from desipipe import Queue, Environment, TaskManager, spawn, setup_logging
 
-sys.path.insert(0, '../')
-import tools
+from clustering_statistics import tools
 
 setup_logging()
 
-queue = Queue('holi_mocks')
+queue = Queue('abacus_mocks')
 queue.clear(kill=False)
 
-output, error = 'slurm_outputs/holi_mocks/slurm-%j.out', 'slurm_outputs/holi_mocks/slurm-%j.err'
+output, error = 'slurm_outputs/abacus_mocks/slurm-%j.out', 'slurm_outputs/abacus_mocks/slurm-%j.err'
 kwargs = {}
 environ = Environment('nersc-cosmodesi') #, command='module swap pyrecon/main pyrecon/mpi')
 #environ = Environment('nersc-cosmodesi')
 tm = TaskManager(queue=queue, environ=environ)
-tm = tm.clone(scheduler=dict(max_workers=25), provider=dict(provider='nersc', time='01:30:00',
+tm = tm.clone(scheduler=dict(max_workers=10), provider=dict(provider='nersc', time='01:30:00',
                             mpiprocs_per_worker=4, output=output, error=error, stop_after=1, constraint='gpu'))
 tm80 = tm.clone(provider=dict(provider='nersc', time='02:00:00',
                             mpiprocs_per_worker=4, output=output, error=error, stop_after=1, constraint='gpu&hbm80g'))
 
 
-def run_stats(tracer='LRG', version='holi-v1-altmtl', imocks=[451], meas_dir=Path(os.getenv('SCRATCH')) / 'measurements', stats=['mesh2_spectrum']):
+def run_stats(tracer='LRG', version='abacus-2ndgen-complete', imocks=[0], stats_dir=Path(os.getenv('SCRATCH')) / 'measurements', stats=['mesh2_spectrum']):
     # Everything inside this function will be executed on the compute nodes;
     # This function must be self-contained; and cannot rely on imports from the outer scope.
     import os
@@ -48,10 +46,7 @@ def run_stats(tracer='LRG', version='holi-v1-altmtl', imocks=[451], meas_dir=Pat
     try: jax.distributed.initialize()
     except RuntimeError: print('Distributed environment already initialized')
     else: print('Initializing distributed environment')
-    sys.path.insert(0, '../')
-    import tools
-    from tools import setup_logging
-    from compute_fiducial_stats import compute_fiducial_stats_from_options, combine_fiducial_stats_from_options, fill_fiducial_options
+    from clustering_statistics import tools, setup_logging, compute_stats_from_options, combine_stats_from_options, fill_fiducial_options
 
     setup_logging()
     cache = {}
@@ -61,40 +56,38 @@ def run_stats(tracer='LRG', version='holi-v1-altmtl', imocks=[451], meas_dir=Pat
         for region in regions:
             options = dict(catalog=dict(version=version, tracer=tracer, zrange=zranges, region=region, imock=imock), mesh2_spectrum={'cut': True, 'auw': True})
             options = fill_fiducial_options(options)
-            compute_fiducial_stats_from_options(stats, get_measurement_fn=functools.partial(tools.get_measurement_fn, meas_dir=meas_dir), cache=cache, **options)
+            compute_stats_from_options(stats, get_stats_fn=functools.partial(tools.get_stats_fn, stats_dir=stats_dir), cache=cache, **options)
         jax.experimental.multihost_utils.sync_global_devices('measurements')
         for region_comb, regions in tools.possible_combine_regions(regions).items():
             _options_imock = dict(options)
             _options_imock['catalog'] = _options_imock['catalog'] | dict(imock=imock)
-            combine_fiducial_stats_from_options(stats, region_comb, regions, get_measurement_fn=functools.partial(tools.get_measurement_fn, meas_dir=meas_dir), **_options_imock)
+            combine_stats_from_options(stats, region_comb, regions, get_stats_fn=functools.partial(tools.get_stats_fn, stats_dir=stats_dir), **_options_imock)
     #jax.distributed.shutdown()
 
 
 if __name__ == '__main__':
 
-    todo = ['exists', 'slurm']
-    #todo = ['exists', 'interactive']
-    #todo = ['interactive']
-    #todo = ['slurm']
-    imocks = np.arange(1001)
+    mode = 'slurm'
+    stats = ['mesh2_spectrum', 'mesh3_spectrum']
+    imocks = np.arange(25)
 
-    meas_dir = Path(os.getenv('SCRATCH')) / 'clustering-measurements-checks_desipipe'
-    version = 'holi-v1-altmtl'
+    stats_dir = Path(os.getenv('SCRATCH')) / 'clustering-measurements-abacus_desipipe'
+    version = 'abacus-2ndgen-complete'
 
-    for tracer in ['LRG', 'ELG_LOPnotqso', 'QSO']:
-        if 'exists' in todo:
+    for tracer in ['LRG', 'ELG', 'QSO']:
+        if True:
             exists, missing = tools.checks_if_exists_and_readable(get_fn=functools.partial(tools.get_catalog_fn, tracer=tracer, region='NGC', version=version), test_if_readable=False, imock=list(range(1001)))[:2]
             imocks = exists[1]['imock']
             rerun = []
             for zrange in tools.propose_fiducial('zranges', tracer):
                 for kind in ['mesh2_spectrum', 'mesh3_spectrum']:
-                    rexists, missing, unreadable = tools.checks_if_exists_and_readable(get_fn=functools.partial(tools.get_measurement_fn, kind=kind, meas_dir=meas_dir, tracer=tracer, region='GCcomb', zrange=zrange, version=version), test_if_readable=True, imock=list(range(1001)))
+                    rexists, missing, unreadable = tools.checks_if_exists_and_readable(get_fn=functools.partial(tools.get_stats_fn, kind=kind, stats_dir=stats_dir, tracer=tracer, region='GCcomb', zrange=zrange, version=version), test_if_readable=True, imock=list(range(1001)))
                     rerun += [imock for imock in imocks if (imock in unreadable[1]['imock']) or (imock not in rexists[1]['imock'])]
             imocks = sorted(set(rerun))
-            batch_imocks = np.array_split(imocks, len(imocks) // 10) if len(imocks) else []
+        batch_imocks = np.array_split(imocks, len(imocks) // 10) if len(imocks) else []
         for _imocks in batch_imocks:
-            if 'interactive' in todo:
-                run_stats(tracer, version=version, imocks=_imocks, meas_dir=meas_dir, stats=['mesh2_spectrum'])
-            elif 'slurm' in todo:
+            if mode == 'interactive':
+                run_stats(tracer, version=version, imocks=_imocks, stats_dir=stats_dir, stats=stats)
+            else:
                 _tm = tm if tracer in ['LRG'] else tm80
-                _tm.python_app(run_stats)(tracer, version=version, imocks=_imocks, meas_dir=meas_dir, stats=['mesh2_spectrum', 'mesh3_spectrum'])
+                _tm.python_app(run_stats)(tracer, version=version, imocks=_imocks, stats_dir=stats_dir, stats=stats)
---
+++ clustering_statistics/job_scripts/desipipe_holi_mocks.py
@@ -0,0 +1,93 @@
+"""
+Script to create and spawn desipipe tasks to compute clustering measurements on HOLI mocks.
+To create and spawn the tasks on NERSC, use the following commands:
+```bash
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+python desipipe_holi_mocks.py  # create the list of tasks
+desipipe tasks -q holi_mocks  # check the list of tasks
+desipipe spawn -q holi_mocks --spawn  # spawn the jobs
+desipipe queues -q holi_mocks  # check the queue
+```
+"""
+import os
+from pathlib import Path
+import functools
+
+import numpy as np
+from desipipe import Queue, Environment, TaskManager, spawn, setup_logging
+
+from clustering_statistics import tools
+
+setup_logging()
+
+queue = Queue('holi_mocks')
+queue.clear(kill=False)
+
+output, error = 'slurm_outputs/holi_mocks/slurm-%j.out', 'slurm_outputs/holi_mocks/slurm-%j.err'
+kwargs = {}
+environ = Environment('nersc-cosmodesi') #, command='module swap pyrecon/main pyrecon/mpi')
+#environ = Environment('nersc-cosmodesi')
+tm = TaskManager(queue=queue, environ=environ)
+tm = tm.clone(scheduler=dict(max_workers=25), provider=dict(provider='nersc', time='01:30:00',
+                            mpiprocs_per_worker=4, output=output, error=error, stop_after=1, constraint='gpu'))
+tm80 = tm.clone(provider=dict(provider='nersc', time='02:00:00',
+                            mpiprocs_per_worker=4, output=output, error=error, stop_after=1, constraint='gpu&hbm80g'))
+
+
+def run_stats(tracer='LRG', version='holi-v1-altmtl', imocks=[451], stats_dir=Path(os.getenv('SCRATCH')) / 'measurements', stats=['mesh2_spectrum']):
+    # Everything inside this function will be executed on the compute nodes;
+    # This function must be self-contained; and cannot rely on imports from the outer scope.
+    import os
+    import sys
+    import functools
+    from pathlib import Path
+    import jax
+    os.environ['XLA_PYTHON_CLIENT_MEM_FRACTION'] = '0.9'
+    try: jax.distributed.initialize()
+    except RuntimeError: print('Distributed environment already initialized')
+    else: print('Initializing distributed environment')
+    from clustering_statistics import tools, setup_logging, compute_stats_from_options, combine_stats_from_options, fill_fiducial_options
+
+    setup_logging()
+    cache = {}
+    zranges = tools.propose_fiducial('zranges', tracer)
+    for imock in imocks:
+        regions = ['NGC', 'SGC']
+        for region in regions:
+            options = dict(catalog=dict(version=version, tracer=tracer, zrange=zranges, region=region, imock=imock), mesh2_spectrum={'cut': True, 'auw': True})
+            options = fill_fiducial_options(options)
+            compute_stats_from_options(stats, get_stats_fn=functools.partial(tools.get_stats_fn, stats_dir=stats_dir), cache=cache, **options)
+        jax.experimental.multihost_utils.sync_global_devices('measurements')
+        for region_comb, regions in tools.possible_combine_regions(regions).items():
+            _options_imock = dict(options)
+            _options_imock['catalog'] = _options_imock['catalog'] | dict(imock=imock)
+            combine_stats_from_options(stats, region_comb, regions, get_stats_fn=functools.partial(tools.get_stats_fn, stats_dir=stats_dir), **_options_imock)
+    #jax.distributed.shutdown()
+
+
+if __name__ == '__main__':
+
+    mode = 'slurm'
+    stats = ['mesh2_spectrum', 'mesh3_spectrum']
+    imocks = np.arange(1001)
+
+    stats_dir = Path(os.getenv('SCRATCH')) / 'clustering-measurements-checks_desipipe'
+    version = 'holi-v1-altmtl'
+
+    for tracer in ['LRG', 'ELG_LOPnotqso', 'QSO']:
+        if True:
+            exists, missing = tools.checks_if_exists_and_readable(get_fn=functools.partial(tools.get_catalog_fn, tracer=tracer, region='NGC', version=version), test_if_readable=False, imock=list(range(1001)))[:2]
+            imocks = exists[1]['imock']
+            rerun = []
+            for zrange in tools.propose_fiducial('zranges', tracer):
+                for kind in ['mesh2_spectrum', 'mesh3_spectrum']:
+                    rexists, missing, unreadable = tools.checks_if_exists_and_readable(get_fn=functools.partial(tools.get_stats_fn, kind=kind, stats_dir=stats_dir, tracer=tracer, region='GCcomb', zrange=zrange, version=version), test_if_readable=True, imock=list(range(1001)))
+                    rerun += [imock for imock in imocks if (imock in unreadable[1]['imock']) or (imock not in rexists[1]['imock'])]
+            imocks = sorted(set(rerun))
+        batch_imocks = np.array_split(imocks, len(imocks) // 10) if len(imocks) else []
+        for _imocks in batch_imocks:
+            if mode == 'interactive':
+                run_stats(tracer, version=version, imocks=_imocks, stats_dir=stats_dir, stats=stats)
+            else:
+                _tm = tm if tracer in ['LRG'] else tm80
+                _tm.python_app(run_stats)(tracer, version=version, imocks=_imocks, stats_dir=stats_dir, stats=stats)
